c6246ea9fb760c167efe61cbbaf9d7368e5e0d9b
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-04-13 15:34:25 +0200

    Catch errors

--- src/app/features/episodes.service.ts
+++ src/app/features/episodes.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { Episodes } from './episodes.entity';
 
-import { combineLatest, throwError, of, BehaviorSubject } from 'rxjs';
-import { catchError, map, shareReplay, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +11,19 @@ import { catchError, map, shareReplay, tap } from 'rxjs/operators';
 export class EpisodesService {
   private episodesUrl = 'https://rickandmortyapi.com/api/episode';
 
-  episodes$ = this.http.get<Episodes>(this.episodesUrl);
-  // .pipe(tap(console.table));
+  episodes$ = this.http
+    .get<Episodes>(this.episodesUrl)
+    .pipe(tap(console.log), catchError(this.handleError));
 
   constructor(private http: HttpClient) {}
+
+  private handleError(err: any) {
+    let errorMessage: string;
+    if (typeof err === 'string') {
+      errorMessage = err;
+    } else {
+      errorMessage = `Backend returned error code ${err.status}: ${err.message}`;
+    }
+    return throwError(errorMessage);
+  }
 }
--- src/app/features/episodes/episode-list/episode-list.component.ts
+++ src/app/features/episodes/episode-list/episode-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { EpisodesService } from '../../episodes.service';
+import {
+  BehaviorSubject,
+  combineLatest,
+  EMPTY,
+  Observable,
+  Subject,
+} from 'rxjs';
+import { catchError, map, pluck } from 'rxjs/operators';
 import { Episodes, ResultsEntity } from '../../episodes.entity';
-
-import { of, BehaviorSubject, Observable, combineLatest } from 'rxjs';
-import { pluck, catchError, map, shareReplay, tap } from 'rxjs/operators';
+import { EpisodesService } from '../../episodes.service';
 
 interface EpisodeSearch {
   name: string;
@@ -32,6 +37,22 @@ interface EpisodeSearch {
           Submit
         </button>
       </form>
+      <div
+        *ngIf="error$ | async as error"
+        class="flex items-center justify-center pa4 bg-light-red navy mt4"
+      >
+        <svg
+          class="w1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 256 256"
+        >
+          <path
+            d="M120 144V96a8 8 0 0116 0v48a8 8 0 01-16 0zm116.768 67.981a23.754 23.754 0 01-20.791 12.011H40.023a24 24 0 01-20.771-36.022l87.977-151.993a24 24 0 0141.543 0l87.976 151.992a23.754 23.754 0 01.02 24.012zM222.9 195.984L134.924 43.992a8 8 0 00-13.848 0L33.1 195.984a8 8 0 006.923 12.008h175.954a8 8 0 006.923-12.008zM128 168a12 12 0 1012 12 12.013 12.013 0 00-12-12z"
+          />
+        </svg>
+        <span class="lh-title ml3">{{ error }}</span>
+      </div>
+
       <section class="flex flex-wrap" *ngIf="vm$ | async as vm">
         <div
           class="br2 ba dark-gray b--black-10 mv4 w-25-l mw5 mr4"
@@ -74,8 +95,14 @@ interface EpisodeSearch {
 })
 export class EpisodeListComponent {
   searchForm;
+  error$ = new Subject<string>();
 
-  episodes$: Observable<Episodes> = this.episodesService.episodes$;
+  episodes$: Observable<Episodes> = this.episodesService.episodes$.pipe(
+    catchError((error) => {
+      this.error$.next(error);
+      return EMPTY;
+    })
+  );
 
   private episodeNameSubject = new BehaviorSubject<string>('');
   episodeNameAction$ = this.episodeNameSubject.asObservable();
@@ -107,5 +134,4 @@ export class EpisodeListComponent {
     this.episodeNameSubject.next(episodeSearch.name);
     this.searchForm.reset();
   }
-
 }
